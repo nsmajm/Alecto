ce7351c1ec3191e2c2fd10fdbf50626c377a2524
nsmajm <nsmajm@gmail.com>
2017-04-04 05:29:09 +0600

    added About Section. style Design and font awesome Icon All are Added

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,13 @@
-
-$('.navbar-nav a').on('click', function () {
-        if (window.width()>0) {
-            $(".navbar-toggle").click();
+$(document).ready(function(){
+ 
+$(window).scroll(function() { 
+        if ($(document).scrollTop() > 100) {  window
+          $(".navbar-fixed-top").css("background-color", "#011217"); 
         }
-    });
+        else {
+          $(".navbar-fixed-top").css("background-color", "transparent"); 
+        }
+      });
+    
+});
+ 
